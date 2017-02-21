1d75ac47f602dff7ebcec673543f251117cc31fc
Chris Coralli <ccoralli@gmail.com>
2017-02-21 17:03:56 -0500

    done.

--- src/js/bonus.js
+++ src/js/bonus.js
@@ -1,54 +1,76 @@
-import {it, expect} from "./test_lib";
+import {it, expect} from './test_lib';
 
 // Constructors
 // Only add code to *THIS* section!
 
-///
-///
-///
-///
+function Human ()    {
+    this.alertness = 0;
+    this.hasCoffee = false;
+    this.needsCoffee = true;
+
+}
+
+Human.prototype.buy = function(coffee)  {
+    this.hasCoffee = true;
+    this.coffee = coffee;
+
+};
+
+function Coffee ()  {
+    this.full = true;
+    this.amount = 3;
+}
+
+Human.prototype.drink = function()    {
+    this.alertness += .33;
+    this.coffee.full = false;
+    this.coffee.amount -= 1;
+    this.coffee.empty = this.coffee.amount === 0;
+
+
+};
 
 // Do not ADD or MODIFY code below this line :D
 
-it("should be sleepy at first", function () {
-  let marie = new Human("Marie");
-  expect(marie.alertness < 0.1).toBe(true);
+it('should be sleepy at first', function () {
+    let marie = new Human('Marie');
+    expect(marie.alertness < 0.1).toBe(true);
 });
 
-it("needs coffee to wake up", function () {
-  let artem = new Human("Artem");
-  expect(artem.hasCoffee).toBe(false);
-  expect(artem.needsCoffee).toBe(true);
+it('needs coffee to wake up', function () {
+    let artem = new Human('Artem');
+    expect(artem.hasCoffee).toBe(false);
+    expect(artem.needsCoffee).toBe(true);
 });
 
-it("can drink coffee to become more alive", function () {
-  let kurt = new Human("Kurt");
-  let omf = new Coffee("Orange Mocha Frappucino");
-  expect(omf.full).toBe(true);
+it('can drink coffee to become more alive', function () {
+    let kurt = new Human('Kurt');
+    let omf = new Coffee('Orange Mocha Frappucino');
+    expect(omf.full).toBe(true);
 
-  kurt.buy(omf);
-  kurt.drink();
-  expect(kurt.alertness > 0.3 && kurt.alertness < 0.4).toBe(true);
-  expect(omf.full).toBe(false);
+    kurt.buy(omf);
+    kurt.drink();
+    expect(kurt.alertness > 0.3 && kurt.alertness < 0.4).toBe(true);
+    expect(omf.full).toBe(false);
 
-  expect(omf.empty).toBe(false);
+    expect(omf.empty).toBe(false);
 });
 
-it("has coffee after buying it", function () {
-  let tyler = new Human("Tyler");
-  let cuban_blend = new Coffee("Fancy Cuban Blend");
-  expect(tyler.hasCoffee).toBe(false);
-  tyler.buy(cuban_blend);
-  expect(tyler.hasCoffee).toBe(true);
+it('has coffee after buying it', function () {
+    let tyler = new Human('Tyler');
+    let cuban_blend = new Coffee('Fancy Cuban Blend');
+    expect(tyler.hasCoffee).toBe(false);
+    tyler.buy(cuban_blend);
+    expect(tyler.hasCoffee).toBe(true);
 });
 
-it("can drink all the coffee", function () {
-  let cory = new Human("Cory");
-  let tsmf = new Coffee("Triple Shot Mocha Frappucino");
-  cory.buy(tsmf);
-  for(let i = 0; i < 3; i++) {
-    cory.drink();
-  }
-  expect(tsmf.empty).toBe(true);
-  expect(cory.alertness > 0.9).toBe(true);
+it('can drink all the coffee', function () {
+    let cory = new Human('Cory');
+    let tsmf = new Coffee('Triple Shot Mocha Frappucino');
+    cory.buy(tsmf);
+    for(let i = 0; i < 3; i++) {
+        cory.drink();
+    }
+    expect(tsmf.empty).toBe(true);
+    expect(cory.alertness > 0.9).toBe(true);
 });
--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,4 +1,9 @@
-function Dog () {
+function Dog (options) {
+    options = options || {};
+    this.color = options.color;
+    this.hungry =  (options.hungry == undefined) ? true : options.hungry;
+    this.status = 'normal';
+    this.owner = options.owner;
 }
 
 export { Dog };
